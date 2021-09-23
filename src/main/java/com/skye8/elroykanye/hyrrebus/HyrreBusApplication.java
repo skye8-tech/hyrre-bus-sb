83af7c276b27d25a15586ca23e37eb0d983e6881
elroykanye <elroykanye@gmail.com>
2021-09-23 12:28:13 +0100

    init commit

---
+++ src/main/java/com/skye8/elroykanye/hyrrebus/HyrreBusApplication.java
@@ -0,0 +1,13 @@
+package com.skye8.elroykanye.hyrrebus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyrreBusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyrreBusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skye8/elroykanye/hyrrebus/HyrreBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skye8.elroykanye.hyrrebus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyrreBusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
