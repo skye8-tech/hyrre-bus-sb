18715bdf46ccb7e56f8d4ee75874ca9b4faa5825
elroykanye <elroykanye@gmail.com>
2021-11-04 23:54:50 +0100

    add global config

--- src/main/java/com/skye8/elroykanye/hyrrebus/config/SecurityConfig.java
+++
@@ -1,7 +0,0 @@
-package com.skye8.elroykanye.hyrrebus.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SecurityConfig {
-}
--- src/main/java/com/skye8/elroykanye/hyrrebus/config/global/SwaggerConfig.java
+++ src/main/java/com/skye8/elroykanye/hyrrebus/config/global/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.skye8.elroykanye.hyrrebus.config;
+package com.skye8.elroykanye.hyrrebus.config.global;
 
 import org.springframework.context.annotation.Configuration;
 
