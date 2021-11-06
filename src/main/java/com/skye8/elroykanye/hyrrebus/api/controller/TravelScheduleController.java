b2d7e336e46fe58c9380eb60871f183ae4f54e6d
andrew21-mch <nfonandrew73@gmail.com>
2021-11-06 01:49:20 +0100

    Revert "Revert "created controllers, services and repositories""
    This reverts commit 98409a6759dff418ac684d6b8a050d9d550e0c8a.

---
+++ src/main/java/com/skye8/elroykanye/hyrrebus/api/controller/TravelScheduleController.java
@@ -0,0 +1,17 @@
+package com.skye8.elroykanye.hyrrebus.api.controller;
+
+import com.skye8.elroykanye.hyrrebus.business.service.TravelScheduleService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Nfon Andrew
+ * created on 06-11-21
+ */
+@RestController
+@AllArgsConstructor
+@RequestMapping(value = "/api/travel_schedule")
+public class TravelScheduleController {
+    private final TravelScheduleService TravelScheduleService;
+}
---
+++ src/main/java/com/skye8/elroykanye/hyrrebus/business/service/impl/TravelScheduleServiceImpl.java
@@ -0,0 +1,16 @@
+package com.skye8.elroykanye.hyrrebus.business.service.impl;
+
+import com.skye8.elroykanye.hyrrebus.data.repository.TravelScheduleRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Nfon Andrew
+ * created on 06-11-21
+ */
+
+@Service
+@AllArgsConstructor
+public class TravelScheduleServiceImpl {
+    private final TravelScheduleRepository TravelScheduleRepository;
+}
