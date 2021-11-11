ce065c99a7465f818b83faea0742087b7a1c0c5f
andrew21-mch <nfonandrew73@gmail.com>
2021-11-11 06:28:35 +0100

    modified mappers

--- src/main/java/com/skye8/elroykanye/hyrrebus/business/mapper/TravelRouteMapper.java
+++ src/main/java/com/skye8/elroykanye/hyrrebus/business/mapper/TravelRouteMapper.java
@@ -1,11 +1,28 @@
 package com.skye8.elroykanye.hyrrebus.business.mapper;
 
+import com.skye8.elroykanye.hyrrebus.api.dto.TravelRouteDto;
+import com.skye8.elroykanye.hyrrebus.api.dto.TravelTicketDto;
+import com.skye8.elroykanye.hyrrebus.data.entity.TravelRoute;
+import com.skye8.elroykanye.hyrrebus.data.entity.TravelTicket;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
 /**
  * @author  Elroy Kanye
  * created on: 06-11-21
+ * Modified by: Nfon Andrew
+ * modified on: 11-11-21
  */
 
 @Mapper(componentModel = "spring", implementationPackage = "<PACKAGE_NAME>.impl")
 public interface TravelRouteMapper {
+    TravelRouteDto mapTravelRouteDtoTo (TravelRoute travelRoute);
+
+    @Mapping(target="bus", ignore = true)
+    @Mapping(target="busDropPoint", ignore = true)
+    @Mapping(target="busBoardPoint", ignore = true)
+    @Mapping(target="travelSchedule", ignore = true)
+    @Mapping(target="travelSchedule", ignore = true)
+    TravelRoute mapDtoToTravelRoute (TravelRouteDto travelRouteDto);
 }
+
--- src/main/java/com/skye8/elroykanye/hyrrebus/business/mapper/TravelTicketMapper.java
+++ src/main/java/com/skye8/elroykanye/hyrrebus/business/mapper/TravelTicketMapper.java
@@ -1,12 +1,24 @@
 package com.skye8.elroykanye.hyrrebus.business.mapper;
 
+import com.skye8.elroykanye.hyrrebus.api.dto.TravelTicketDto;
+import com.skye8.elroykanye.hyrrebus.data.entity.TravelTicket;
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 /**
  * @author Elroy Kanye
  * created on: 06-11-21
+ * modified by Nfon Andrew
+ * modified on: 11-11-21
  */
 
 @Mapper (componentModel = "spring", implementationPackage = "<PACKAGE_NAME>.impl")
 public interface TravelTicketMapper {
+    TravelTicketDto mapTravelTicketToDto (TravelTicket travelTicket);
+
+    @Mapping(target="travelSchedule", ignore = true)
+    TravelTicket mapDtoToTravelTicket (TravelTicketDto travelTicketDto);
+
+
 }
