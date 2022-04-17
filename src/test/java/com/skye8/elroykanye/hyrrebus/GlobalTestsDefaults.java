1b7dcdfb60f98acf72673d631d546d7121ca7221
Elroy Kanye <elroykanye@gmail.com>
2022-04-17 01:38:45 +0100

    update GlobalTestsDefaults

--- src/test/java/com/skye8/elroykanye/hyrrebus/GlobalTestsDefaults.java
+++ src/test/java/com/skye8/elroykanye/hyrrebus/GlobalTestsDefaults.java
@@ -3,9 +3,12 @@ package com.skye8.elroykanye.hyrrebus;
 import com.skye8.elroykanye.hyrrebus.api.dto.AgencyAddressDto;
 import com.skye8.elroykanye.hyrrebus.api.dto.AgencyDto;
 import com.skye8.elroykanye.hyrrebus.api.dto.AgencyUserDto;
+import com.skye8.elroykanye.hyrrebus.api.dto.BusDto;
 import com.skye8.elroykanye.hyrrebus.data.entity.Agency;
 import com.skye8.elroykanye.hyrrebus.data.entity.AgencyAddress;
 import com.skye8.elroykanye.hyrrebus.data.entity.AgencyUser;
+import com.skye8.elroykanye.hyrrebus.data.entity.Bus;
+import com.skye8.elroykanye.hyrrebus.data.entity.enums.BusType;
 
 public class GlobalTestsDefaults {
     public static final Agency DEFAULT_AGENCY = Agency.builder()
@@ -27,6 +30,16 @@ public class GlobalTestsDefaults {
             .build();
 
     public static final AgencyUserDto DEFAULT_AGENCY_USER_DTO = AgencyUserDto.builder()
-            .agencyUserId(1L).username("user").firstName("First").lastName("Last").phoneNumber("1234567890")
+            .agencyUserId(1L).username("userdto").firstName("FirstDto").lastName("LastDto").phoneNumber("1234567890")
+            .build();
+
+    public static final Bus DEFAULT_BUS = Bus.builder()
+            .busId(1L).busCapacity(70).numberOfRows(14).numberOfColumns(5).busType(BusType.STANDARD)
+            .plateNumber("ABC123").busName("My Bus").busNumber(123L)
+            .build();
+
+    public static final BusDto DEFAULT_BUS_DTO = BusDto.builder()
+            .busId(1L).busCapacity(70).numberOfRows(14).numberOfColumns(5).busType("standard")
+            .plateNumber("ABC123DTO").busName("My BusDto").busNumber(123L)
             .build();
 }
