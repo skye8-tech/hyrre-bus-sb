16037107047492eddc3aace7154ab2fa05639495
Elroy Kanye <elroykanye@gmail.com>
2022-04-17 01:39:18 +0100

    update the BusType enum

--- src/main/java/com/skye8/elroykanye/hyrrebus/data/entity/enums/BusType.java
+++ src/main/java/com/skye8/elroykanye/hyrrebus/data/entity/enums/BusType.java
@@ -5,7 +5,21 @@ public enum BusType {
     STANDARD("standard"),
     COASTAL("coastal");
 
-    BusType(String typeName) {
+    private final String typeName;
 
+    BusType(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+    public BusType getBusType(String typeName) {
+        for (BusType type : BusType.values()) {
+            if (type.name().equals(typeName)) {
+                return type;
+            }
+        }
+        return null;
     }
 }
